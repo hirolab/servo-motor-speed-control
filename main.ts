bee5b18ad1073ee40251360f26b4b67ee7a4b72b
lifanwu <keystoradio@gmail.com>
2022-02-25 21:27:48 -0500

    Change servo motor to function and set initial value

--- main.ts
+++ main.ts
@@ -1,22 +1,42 @@
-let shoulder_pos = 0
 let angle = 60
 let max_angle = 160
 let min_angle = 20
-basic.forever(function () {
-    shoulder_pos = input.acceleration(Dimension.X)
-    if (Math.abs(shoulder_pos) >= 100) {
-        if (shoulder_pos <= 0) {
-            angle = angle - 3
-        } else {
-            angle = angle + 3
+
+namespace SetServoSpeed{
+    export function SetTurnSpeed(direction: number, speed: number): void{
+        if (Math.abs(direction) >= 100) {
+            if (direction <= 0) {
+                angle = angle - 3
+            } else {
+                angle = angle + 3
+            }
+            if (angle <= min_angle) {
+                angle = min_angle
+            }
+            if (angle >= max_angle) {
+                angle = max_angle
+            }
+            servos.P0.setAngle(angle)
         }
-        if (angle <= min_angle) {
-            angle = min_angle
-        }
-        if (angle >= max_angle) {
-            angle = max_angle
-        }
-        servos.P0.setAngle(angle)
+        basic.pause(speed)
     }
-    basic.pause(2)
-})
+}
+
+// basic.forever(function () {
+//     shoulder_pos = input.acceleration(Dimension.X)
+//     if (Math.abs(shoulder_pos) >= 100) {
+//         if (shoulder_pos <= 0) {
+//             angle = angle - 3
+//         } else {
+//             angle = angle + 3
+//         }
+//         if (angle <= min_angle) {
+//             angle = min_angle
+//         }
+//         if (angle >= max_angle) {
+//             angle = max_angle
+//         }
+//         servos.P0.setAngle(angle)
+//     }
+//     basic.pause(2)
+// })
--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+let shoulder_pos = 0
+
+
+basic.forever(function () {
+    shoulder_pos = input.acceleration(Dimension.X)
+    SetServoSpeed.SetTurnSpeed(shoulder_pos,2)
+})
